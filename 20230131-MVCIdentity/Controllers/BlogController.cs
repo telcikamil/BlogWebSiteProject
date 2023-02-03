bd5fe0c841b68ba25bb137e2169444b7390e0afc
telcikamil <kamiltelci@outlook.com>
2023-02-03 21:07:35 +0300

    a

---
+++ 20230131-MVCIdentity/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _20230131_MVCIdentity.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
